49f3c2bc1eaabe50713dc64f3cb71c05c37ffa4d
Tyron <tyronsamaroo@gmail.com>
2020-03-17 00:00:15 -0400

    Added documentation, completed code for the problem, added exception handeling

---
+++ main.py
@@ -0,0 +1,33 @@
+import sys
+
+
+def one_to_one_char_map(s1,s2):
+    """This function determine whether a one-to-one character mapping exist from one string, s1, to another s2. 
+    
+    Arguments:
+        s1 {String} -- string of english alphabet letters first command line argument 
+        s2 {String} -- string of english alphabet letters second command line argument
+    
+    Returns:
+        [Boolean] -- True if mapping exist otherwise False 
+    """
+    if not (s1.isalpha() and s2.isalpha()):
+        raise TypeError("Inputs are not from english alphabet")   
+    #First s1 and s1 has to be same length 
+    if len(s1) != len(s2):
+        return False
+
+    #This step the len should be the same so now
+    #Second string s1 cannot has duplicate characters
+    if len(s1) != len(set(s1)):
+        return False
+    
+    return True
+    
+    
+if __name__ == "__main__":
+    try:
+        print(one_to_one_char_map(sys.argv[1],sys.argv[2]))
+    except IndexError:
+        print("You did not list two valid inputs")
+   
