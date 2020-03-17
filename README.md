75928756d5b8d5c53c358a177fd94a1ad97397db
Tyron <tyronsamaroo@gmail.com>
2020-03-17 00:23:33 -0400

    main.py: Added a -h flag to retrive information about function ReadME.md: Adding information relevent to the program

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # kg_tyronsamaroo_2020
+
+
+#### In order to run program please make sure you are using at least Python 3.8.0
+
+
+## Requirements and Constraints
+1. You are only allowed to pass two command line arguments
+2. Arguments can only be from the english alphabet
+3. Program is case sensitive `A` is not same as `a`
+
+
+## How to run code
+`python3 main.py [arg1] [arg2]`
+Example: `python3 main.py abc abc`
+
+# Additional Information 
+
+I have added exception handeling and provided a python docstring which has information about given function.
+
+
+
+
+
+# Function Info
+You can run `python3 main.py -h` to find out more. `-h` is a flag you can pass in order to recieve this information
+
+
+
+# Resource 
+
+Download Python3 -> https://www.python.org/downloads/
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -9,10 +9,11 @@ def one_to_one_char_map(s1,s2):
         s2 {String} -- string of english alphabet letters second command line argument
     
     Returns:
-        [Boolean] -- True if mapping exist otherwise False 
+        Boolean -- True if mapping exist otherwise False 
     """
     if not (s1.isalpha() and s2.isalpha()):
         raise TypeError("Inputs are not from english alphabet")   
+    
     #First s1 and s1 has to be same length 
     if len(s1) != len(s2):
         return False
@@ -22,12 +23,16 @@ def one_to_one_char_map(s1,s2):
     if len(s1) != len(set(s1)):
         return False
     
+    #At this point s1 and s1 should be from english alphabet, same length and s1 is unique so it can map to s2 which has enough letters to map each. 
     return True
     
     
 if __name__ == "__main__":
+    if sys.argv[1] == '-h':
+        print(help(one_to_one_char_map))
     try:
         print(one_to_one_char_map(sys.argv[1],sys.argv[2]))
     except IndexError:
         print("You did not list two valid inputs")
+        
    
